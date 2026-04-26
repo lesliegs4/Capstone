ac6627047eaed0529e235d0127ff3bf8b420dd3f
Leslie Garcia-Sanchez <leslieg@usc.edu>
2026-04-25 19:00:11 -0700

    joystick movements

--- mail_test/mail_test.ino
+++ mail_test/mail_test.ino
@@ -18,6 +18,10 @@ SMTPSession smtp;
 
 #define RECIPIENT_EMAIL "lesliegs409@gmail.com"
 
+#define JOY_X 0      // A0 / GPIO0
+#define JOY_Y 1      // A1 / GPIO1
+#define JOY_BTN 2    // GPIO2
+
 void msg(const char* a, const char* b = "") {
   d.clearDisplay();
   d.setCursor(0, 18);
@@ -29,7 +33,8 @@ void msg(const char* a, const char* b = "") {
 }
 
 void fish(int x, int y) {
-  d.fillTriangle(x, y, x+7, y-4, x+7, y+4, WHITE);
+  // d.fillTriangle(x, y, x+7, y-4, x+7, y+4, WHITE);
+  d.fillTriangle(x - 2, y, x + 7, y - 4, x + 24, y + 4, WHITE);
   d.fillRoundRect(x+7, y-5, 17, 10, 5, WHITE);
   d.fillCircle(x+20, y-2, 1, BLACK);
 }
@@ -48,7 +53,7 @@ void sendMail() {
   m.sender.email = AUTHOR_EMAIL;
   m.subject = "ESP32 Alert";
   m.addRecipient("Leslie", RECIPIENT_EMAIL);
-  m.text.content = "ESP32 test alert sent successfully.";
+  m.text.content = "Este es Caramello y ESP32 dejandole saber que sus pescados necessitan ayuda. Por favor.";
 
   if (!smtp.connect(&s)) {
     msg("SMTP failed");
@@ -81,16 +86,72 @@ void setup() {
   msg("WiFi connected!");
   delay(1000);
   sendMail();
+
+  pinMode(JOY_X, INPUT);
+  pinMode(JOY_Y, INPUT);
+  pinMode(JOY_BTN, INPUT_PULLUP);
+
+}
+
+void showJoystick() {
+  int x = analogRead(JOY_X);
+  int y = analogRead(JOY_Y);
+  int btn = digitalRead(JOY_BTN);
+
+  d.clearDisplay();
+  d.setTextSize(1);
+  d.setTextColor(WHITE);
+
+  d.setCursor(0, 0);
+  d.println("Joystick:");
+
+  if (btn == LOW) {
+    d.setCursor(38, 52);
+    d.println("PRESS");
+  }
+
+    // Rotated mapping:
+  // physical LEFT  -> OLED UP
+  // physical UP    -> OLED RIGHT
+  // physical RIGHT -> OLED DOWN
+  // physical DOWN  -> OLED LEFT
+
+  if (x < 1200) { // up
+    d.setCursor(60, 18);
+    d.println("^");
+  } 
+  else if (y < 1200) { // right
+    d.setCursor(65, 28);
+    d.println(">");
+  } 
+  else if (x > 2800) { // down
+    d.setCursor(60, 38);
+    d.println("v");
+  } 
+  else if (y > 2800) { // left
+    d.setCursor(55, 28);
+    d.println("<");
+  } 
+  else {
+    fish(45, 32);
+  }
+
+  d.display();
 }
 
 void loop() {
-  for (int x = -30; x < 130; x += 5) {
-    d.clearDisplay();
-    d.setCursor(20, 5);
-    d.println("Idle");
-    fish(x, 32);
-    fish(x - 45, 48);
-    d.display();
-    delay(130);
-  }
+  showJoystick();
+  delay(120);
 }
+
+// void loop() {
+//   for (int x = -30; x < 130; x += 5) {
+//     d.clearDisplay();
+//     d.setCursor(20, 5);
+//     d.println("Idle");
+//     fish(x, 32);
+//     fish(x - 45, 48);
+//     d.display();
+//     delay(130);
+//   }
+// }
